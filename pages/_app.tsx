7f25a4dbbd7ea3b638ac520e4f4bc7bc5c366a5c
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-11-06 21:37:46 -0600

    prepping for vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '../context/AuthContext'
+import { AuthProvider } from 'context/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
