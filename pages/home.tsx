2241444d841171d225ed56ddd63cb711b9400a8f
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-11-11 20:07:36 -0600

    fixed name text

--- pages/home.tsx
+++ pages/home.tsx
@@ -44,31 +44,31 @@ const Home: NextPage | any = () => {
   return (
     <>
       <nav className="flex justify-end items-center flex-wrap text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed relative bg-gradient-to-r from-emerald-500 via-emerald-600 to-teal-600 shadow-sm p-4">
-          <Menu>
-            <Menu.Button className="p-2 border-2 border-white rounded-md hover:bg-white transition-colors duration-75">
-              <div className="inline-block">
-                {userData?.firstName}
-                <ChevronDownIcon className="w-4 h-4 inline" />
-              </div>
-            </Menu.Button>
-            <Menu.Items>
-              <Menu.Item>
-                {({ active }) => (
-                  <a href="/profile">Profile</a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="/"
-                    onClick={handleLogout}
-                  >
-                    Log Out
-                  </a>
-                )}
-              </Menu.Item>
-            </Menu.Items>
-          </Menu>
+        <Menu>
+          <Menu.Button className="p-2 border-2 border-white rounded-md hover:bg-white hover:text-black transition-colors duration-75">
+            <div className="inline-block">
+              {userData?.firstName}
+              <ChevronDownIcon className="w-4 h-4 inline" />
+            </div>
+          </Menu.Button>
+          <Menu.Items>
+            <Menu.Item>
+              {({ active }) => (
+                <a href="/profile">Profile</a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="/"
+                  onClick={handleLogout}
+                >
+                  Log Out
+                </a>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Menu>
       </nav>
     </>
   )
