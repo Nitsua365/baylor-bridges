8524a381d8d40ba14fde0d92119609944c18047c
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-11-13 21:53:37 -0600

    adding progress bar on page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from 'context/AuthContext'
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <AuthProvider>
+      <NextNProgress />
       <Component {...pageProps} />
     </AuthProvider>
   )
