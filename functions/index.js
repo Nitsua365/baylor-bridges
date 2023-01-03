f1af6a2787a5e9dfc803de0dc3d241389e27434a
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-01-03 17:34:39 -0600

    added INFO and ERROR logging to cloud functions

--- functions/index.js
+++ functions/index.js
@@ -11,22 +11,53 @@ const UserIndex = MeiliClient.index("users");
 exports.addToMeiliIndex = functions.firestore.document("/users/{uid}")
 
   .onCreate(snapshot => {
-    const newData = snapshot.data();
-    const uid = snapshot.id;
+    try {
+      const newData = snapshot.data();
+      const uid = snapshot.id;
 
-    return UserIndex.addDocuments([{ uid, ...newData }])
+      const res = UserIndex.addDocuments([{ uid, ...newData }])
+
+      functions.logger.info(`[${(new Date()).toString()}] CREATED MEILISEARCH DOCUMENT [User Id]: ${uid}`)
+
+      return res
+    }
+    catch (e) {
+      functions.logger.error(`[${(new Date()).toString()}] CREATED MEILISEARCH DOCUMENT [User Id]: ${snapshot.id} ERROR: ${e.toString()}`)
+    }
+    
   })
 
 exports.updateToMeiliIndex = functions.firestore.document("/users/{uid}")
 
   .onUpdate(snapshot => {
-    const newData = snapshot.after.data()
-    const uid = snapshot.after.id
+    try {
+      const newData = snapshot.after.data()
+      const uid = snapshot.after.id
 
-    return UserIndex.updateDocuments([{ uid, ...newData }])
+      const res = UserIndex.updateDocuments([{ uid, ...newData }])
+
+      functions.logger.info(`[${(new Date()).toString()}] UPDATED MEILISEARCH DOCUMENT [User Id]: ${uid}`)
+
+      return res
+    }
+    catch (e) {
+      functions.logger.error(`[${(new Date()).toString()}] UPDATED MEILISEARCH DOCUMENT [User Id]: ${snapshot.after.id} ERROR: ${e.toString()}`)
+    }
   })
 
 
 exports.deleteFromMeiliIndex = functions.firestore.document("/users/{uid}")
 
-  .onDelete(snapshot => UserIndex.deleteDocument(snapshot.id))
+  .onDelete(snapshot => {
+    try {
+      const res = UserIndex.deleteDocument(snapshot.id)
+
+      functions.logger.info(`[${(new Date()).toString()}] DELETED MEILISEARCH DOCUMENT [User Id]: ${snapshot.id}`)
+
+      return res
+    }
+    catch (e) {
+      functions.logger.error(`[${(new Date()).toString()}] UPDATED MEILISEARCH DOCUMENT [User Id]: ${snapshot.id} ERROR: ${e.toString()}`)
+    }
+    
+  })
